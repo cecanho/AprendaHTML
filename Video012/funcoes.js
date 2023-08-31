8b2eabdd78d78a00f27afc288dcacf85b4f2c4ec
Cristiano José Cecanho <cecanho@gmail.com>
2023-08-31 20:37:08 -0300

    JAVAScript, file path e head

---
+++ Video012/funcoes.js
@@ -0,0 +1,5 @@
+function dataAgora(){
+			document.getElementById('dataHora').innerHTML = Date();
+			document.getElementById('dataHora').style.fontSize = "25px";
+			document.getElementById('dataHora').style.color = "red";
+		}
